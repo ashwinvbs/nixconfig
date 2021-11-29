cef295d0a9d66d100dcae8a8b8c7855e8971433b
Ashwin Balasubramaniyan <ashwin.vbs@gmail.com>
2021-11-29 16:51:33 +0000

    Remove commented lines

--- components/gnome.nix
+++ components/gnome.nix
@@ -7,9 +7,6 @@
   # Configure keymap in X11
   services.xserver.layout = "us";
 
-  # Enable touchpad support (enabled default in most desktopManager).
-  # services.xserver.libinput.enable = true;
-
   # Enable the GNOME Desktop Environment.
   services.xserver.displayManager.gdm.enable = true;
   services.xserver.desktopManager.gnome.enable = true;
@@ -27,8 +24,4 @@
     google-chrome
     gnome.nautilus
   ];
-
-  # Enable sound.
-  # sound.enable = true;
-  # hardware.pulseaudio.enable = true;
 }
