c4d048bdfce7246ad66c236ee7cd1a21985fc64b
Ashwin Balasubramaniyan <ashwin.vbs@gmail.com>
2021-11-29 08:01:54 +0000

    Move sshd config to a separate file

--- components/nuc-config.nix
+++ components/nuc-config.nix
@@ -1,21 +1,9 @@
 { config, pkgs, ... }:
 
 {
-  networking.hostName = "nuc"; # Define your hostname.
-
-  # Enable CUPS to print documents.
-  services.printing.enable = false;
+  networking.hostName = "nuc";
 
   networking.useDHCP = false;
   networking.interfaces.eno1.useDHCP = true;
   networking.interfaces.wlp0s20f3.useDHCP = true;
-
-  # Enable the OpenSSH daemon.
-  # services.openssh.enable = true;
-
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
 }
