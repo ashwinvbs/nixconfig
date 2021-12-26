fbbda9f7763d1bd471d85f4655a347408e9d92f1
Ashwin Balasubramaniyan <ashwin.vbs@gmail.com>
2021-12-26 09:53:00 +0000

    Fix duplicate declarations

--- components/ssh-host.nix
+++ components/ssh-host.nix
@@ -18,9 +18,4 @@ in {
       "/etc/ssh/ssh_host_ed25519_key.pub"
     ];
   };
-
-  environment.etc."ssh/ssh_host_rsa_key".source         = "/state/sshd/etc/ssh/ssh_host_rsa_key";
-  environment.etc."ssh/ssh_host_rsa_key.pub".source     = "/state/sshd/etc/ssh/ssh_host_rsa_key.pub";
-  environment.etc."ssh/ssh_host_ed25519_key".source     = "/state/sshd/etc/ssh/ssh_host_ed25519_key";
-  environment.etc."ssh/ssh_host_ed25519_key.pub".source = "/state/sshd/etc/ssh/ssh_host_ed25519_key.pub";
 }
