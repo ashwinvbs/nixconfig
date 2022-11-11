c1f7d95e31bc09b37f49b904c2f953b8fc673841
Ashwin Balasubramaniyan <ashwin.vbs@gmail.com>
2022-11-11 06:05:17 +0000

    Re-enable suspend for evaluation

--- components/common/base.nix
+++ components/common/base.nix
@@ -8,15 +8,6 @@
 
   i18n.defaultLocale = "en_US.UTF-8";
 
-  # Disable sleep suspend and hibernate.
-  # Power management is wonky at times and broken at worst
-  systemd.targets = {
-    sleep.enable = false;
-    suspend.enable = false;
-    hibernate.enable = false;
-    hybrid-sleep.enable = false;
-  };
-
   # https://github.com/NixOS/nixpkgs/issues/87802
   boot.kernelParams = [ "ipv6.disable=1" ];
   networking.enableIPv6 = false;
