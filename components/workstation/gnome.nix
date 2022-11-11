199c7c29645b50c057d983ed6abe71d5d04c82aa
Ashwin Balasubramaniyan <ashwin.vbs@gmail.com>
2022-11-11 05:49:00 +0000

    Re-add most of gnome for better shell integration

--- components/workstation/gnome.nix
+++ components/workstation/gnome.nix
@@ -20,87 +20,46 @@ in
 
   # Enable the GNOME Desktop Environment.
   services.xserver.displayManager.gdm.enable = true;
-  services.xserver.displayManager.sessionPackages = [ pkgs.gnome.gnome-session.sessions ];
+  services.xserver.desktopManager.gnome.enable = true;
 
-  hardware.bluetooth.enable = true;
-  hardware.pulseaudio.enable = true;
-  networking.networkmanager.enable = true;
-  programs.dconf.enable = true;
-  security.polkit.enable = true;
-  services.accounts-daemon.enable = true;
-  services.gnome.gnome-keyring.enable = true;
-  services.hardware.bolt.enable = true;
-  services.power-profiles-daemon.enable = true;
-  services.udisks2.enable = true;
-  services.upower.enable = config.powerManagement.enable;
-  services.xserver.libinput.enable = true; # for controlling touchpad settings via gnome control center
-  services.xserver.updateDbusEnvironment = true;
-  nixpkgs.config.vim.gui = "gtk3";
-  services.gnome.glib-networking.enable = true;
-  services.gnome.gnome-settings-daemon.enable = true;
-  services.gvfs.enable = true;
+  # gnome.core-os-services overrides
+  services.dleyna-renderer.enable = false;
+  services.dleyna-server.enable = false;
+  services.gnome.gnome-online-accounts.enable = false;
+  services.gnome.tracker-miners.enable = false;
+  services.gnome.tracker.enable = false;
+  # Would like to disable but cannot
+  # services.gnome.at-spi2-core.enable = true;
+  # services.gnome.evolution-data-server.enable = true;
+  # services.gnome.gnome-online-miners.enable = true;
 
-  services.udev.packages = with pkgs.gnome; [
-    # Force enable KMS modifiers for devices that require them.
-    # https://gitlab.gnome.org/GNOME/mutter/-/merge_requests/1443
-    mutter
+  # gnome.core-shell overrides
+  services.gnome.gnome-initial-setup.enable = false;
+  services.gnome.gnome-remote-desktop.enable = false;
+  services.gnome.gnome-user-share.enable = false;
+  services.gnome.rygel.enable = false;
+  services.system-config-printer.enable = false;
+  services.avahi.enable = false;
+
+  environment.gnome.excludePackages = [
+    pkgs.gnome-tour
+    pkgs.gnome-user-docs
+    pkgs.orca
   ];
 
-  # Explicitly enabled since GNOME will be severely broken without these.
-  xdg.mime.enable = true;
-  xdg.icons.enable = true;
-
-  # Harmonize Qt5 application style and also make them use the portal for file chooser dialog.
-  qt5 = {
-    enable = true;
-    platformTheme = "gnome";
-    style = "adwaita";
-  };
-  fonts.fonts = with pkgs; [
-    cantarell-fonts
-    dejavu_fonts
-    source-code-pro # Default monospace font in 3.32
-    source-sans
-  ];
-
-  services.geoclue2.enable = true;
-  services.geoclue2.enableDemoAgent = false; # GNOME has its own geoclue agent
-
-  services.geoclue2.appConfig.gnome-datetime-panel = {
-    isAllowed = true;
-    isSystem = true;
-  };
-  services.geoclue2.appConfig.gnome-color-panel = {
-    isAllowed = true;
-    isSystem = true;
-  };
-  services.geoclue2.appConfig."org.gnome.Shell" = {
-    isAllowed = true;
-    isSystem = true;
-  };
-
-  # Needed for themes and backgrounds
-  environment.pathsToLink = [
-    "/share" # TODO: https://github.com/NixOS/nixpkgs/issues/47173
-  ];
-
+  # disable gnome.core-utilities and include minimal replacements
+  services.gnome.core-utilities.enable = false;
   environment.systemPackages = with pkgs.gnome; [
-    adwaita-icon-theme
-    gnome-backgrounds
-    gnome-bluetooth
-    gnome-control-center
-    gnome-themes-extra
     nautilus
-    pkgs.glib # for gsettings program
     pkgs.gnome-console
-    pkgs.gnome-menus
     pkgs.gnome-text-editor
-    pkgs.gtk3.out # for gtk-launch program
-    pkgs.xdg-user-dirs # Update user dirs as described in http://freedesktop.org/wiki/Software/xdg-user-dirs/
   ];
+
+  programs.file-roller.enable = true;
 
   # VTE shell integration for gnome-console
   programs.bash.vteIntegration = true;
+
   # Override default mimeapps for nautilus
   environment.sessionVariables.XDG_DATA_DIRS = [ "${mimeAppsList}/share" ];
 }
