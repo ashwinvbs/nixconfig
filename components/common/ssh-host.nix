b0f213c134b0fc850f0f95173600575f57a52935
Ashwin Balasubramaniyan <ashwin.vbs@gmail.com>
2022-11-25 18:29:43 +0000

    Update ssh server attrib name

--- components/common/ssh-host.nix
+++ components/common/ssh-host.nix
@@ -10,7 +10,7 @@ in {
   services.openssh = {
     enable = true;
     passwordAuthentication = false;
-    challengeResponseAuthentication = false;
+    kbdInteractiveAuthentication = false;
     permitRootLogin = "no";
   };
 
