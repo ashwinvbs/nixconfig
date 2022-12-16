3eb8ea7b13ac5a8f199cf180b4c8241471e1a5af
Ashwin Balasubramaniyan <ashwin.vbs@gmail.com>
2022-12-16 21:33:31 +0000

    Attempt to use x11 instead of wayland

--- components/workstation/gnome.nix
+++ components/workstation/gnome.nix
@@ -20,6 +20,7 @@ in
 
   # Enable the GNOME Desktop Environment.
   services.xserver.displayManager.gdm.enable = true;
+  services.xserver.displayManager.gdm.wayland = false;
   services.xserver.desktopManager.gnome.enable = true;
 
   # gnome.core-os-services overrides
