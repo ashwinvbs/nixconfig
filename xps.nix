7046ceb89597cc5636dd0aad67b46641882c428d
Ashwin Balasubramaniyan <ashwin.vbs@gmail.com>
2023-05-06 00:48:45 -0400

    Have additional users listed in the machine where they are needed

--- xps.nix
+++ xps.nix
@@ -4,8 +4,12 @@
   imports = [
     ./hardware/intel.nix
     ./components/workstation.nix
-
-    ./components/misc/additional-users.nix
   ];
   networking.hostName = "xps";
+
+  users.users.radhulya = {
+    isNormalUser = true;
+    description = "Radhulya Thirumalaisamy";
+    hashedPassword = lib.strings.fileContents /etc/nixos/secrets/radpass.txt;
+  };
 }
