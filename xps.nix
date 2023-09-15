1e64ef4b7d1d86fa370567f00ad96f56f29f11ce
Ashwin Balasubramaniyan <ashwin.vbs@gmail.com>
2023-09-15 23:14:10 +0000

    Persist files in secondary user in xps

--- xps.nix
+++ xps.nix
@@ -4,6 +4,7 @@
   imports = [
     ./hardware/intel.nix
     ./components/workstation.nix
+    "${builtins.fetchTarball { url = "https://github.com/nix-community/impermanence/archive/master.tar.gz"; }}/nixos.nix"
   ];
   networking.hostName = "xps";
 
@@ -11,5 +12,23 @@
     isNormalUser = true;
     description = "Radhulya Thirumalaisamy";
     hashedPassword = lib.strings.fileContents /etc/nixos/secrets/radpass.txt;
+  };
+
+  environment.persistence."/nix/state" = {
+    hideMounts = true;
+    users.radhulya = {
+      directories = [
+        ".config"
+        ".local"
+        "Documents"
+        "Downloads"
+        "Workspaces"
+      ];
+      files = [
+        ".bash_history"
+        ".bashrc"
+        ".gitconfig"
+      ];
+    };
   };
 }
