1d0e00d7e2c96064bf17bd01703290c982c1d5ca
Ashwin Balasubramaniyan <ashwin.vbs@gmail.com>
2024-02-24 05:48:10 +0000

    Make gaming.nix self sufficient

--- components/gaming.nix
+++ components/gaming.nix
@@ -4,6 +4,8 @@
   options.installconfig.components.gaming = lib.mkEnableOption "Enables software for gaming";
 
   config = lib.mkIf config.installconfig.components.gaming {
+    nixpkgs.config.allowUnfree = true;
+
     environment.systemPackages = with pkgs; [
       heroic
       scanmem
