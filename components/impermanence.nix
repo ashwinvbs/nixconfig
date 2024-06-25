b7f59f08ff8933358257c9095dbe712214a03a65
Ashwin Balasubramaniyan <ashwin.vbs@gmail.com>
2024-06-25 04:03:50 +0000

    Use merge to make persistence list less verbose

--- components/impermanence.nix
+++ components/impermanence.nix
@@ -10,16 +10,51 @@
       # File system defines
       fileSystems."/".options = [ "defaults" "size=2G" "mode=755" ];
 
-      environment.persistence."/nix/state" = {
-        hideMounts = true;
-        directories = [
-          "/etc/nixos"
-          "/var/log"
-        ];
-        files = [
-          "/etc/machine-id"
-        ];
-      };
+      environment.persistence."/nix/state" = lib.mkMerge [
+        ( {
+          hideMounts = true;
+          directories = [
+            "/etc/nixos"
+            "/var/log"
+          ];
+          files = [
+            "/etc/machine-id"
+          ];
+        } )
+
+        ( lib.mkIf config.virtualisation.libvirtd.enable {
+          directories = [ "/var/lib/libvirt" ];
+        } )
+
+        ( lib.mkIf config.services.fprintd.enable {
+          directories = [ "/var/lib/fprint" ];
+        } )
+
+        ( lib.mkIf config.services.flatpak.enable {
+          directories = [ "/var/lib/flatpak" ];
+        } )
+
+        ( lib.mkIf config.virtualisation.docker.enable {
+          directories = [ "/var/lib/docker" ];
+        } )
+
+        ( lib.mkIf config.hardware.bluetooth.enable {
+          directories = [ "/var/lib/bluetooth" ];
+        } )
+
+        ( lib.mkIf config.networking.networkmanager.enable {
+          directories = [ "/etc/NetworkManager/system-connections" ];
+        } )
+
+        ( lib.mkIf config.services.openssh.enable {
+          files = [
+            "/etc/ssh/ssh_host_rsa_key"
+            "/etc/ssh/ssh_host_rsa_key.pub"
+            "/etc/ssh/ssh_host_ed25519_key"
+            "/etc/ssh/ssh_host_ed25519_key.pub"
+          ];
+        } )
+      ];
     } )
 
     ( lib.mkIf config.services.tailscale.enable {
@@ -30,53 +65,6 @@
       systemd.tmpfiles.rules = [
         "d /nix/state/var/lib/tailscale 0700 root root"
       ];
-    } )
-
-    ( lib.mkIf config.virtualisation.libvirtd.enable {
-      environment.persistence."/nix/state" = {
-        directories = [ "/var/lib/libvirt" ];
-      };
-    } )
-
-    ( lib.mkIf config.services.fprintd.enable {
-      environment.persistence."/nix/state" = {
-        directories = [ "/var/lib/fprint" ];
-      };
-    } )
-
-    ( lib.mkIf config.services.flatpak.enable {
-      environment.persistence."/nix/state" = {
-        directories = [ "/var/lib/flatpak" ];
-      };
-    } )
-
-    ( lib.mkIf config.virtualisation.docker.enable {
-      environment.persistence."/nix/state" = {
-        directories = [ "/var/lib/docker" ];
-      };
-    } )
-
-    ( lib.mkIf config.hardware.bluetooth.enable {
-      environment.persistence."/nix/state" = {
-        directories = [ "/var/lib/bluetooth" ];
-      };
-    } )
-
-    ( lib.mkIf config.networking.networkmanager.enable {
-      environment.persistence."/nix/state" = {
-        directories = [ "/etc/NetworkManager/system-connections" ];
-      };
-    } )
-
-    ( lib.mkIf config.services.openssh.enable {
-      environment.persistence."/nix/state" = {
-        files = [
-          "/etc/ssh/ssh_host_rsa_key"
-          "/etc/ssh/ssh_host_rsa_key.pub"
-          "/etc/ssh/ssh_host_ed25519_key"
-          "/etc/ssh/ssh_host_ed25519_key.pub"
-        ];
-      };
     } )
   ];
 }
