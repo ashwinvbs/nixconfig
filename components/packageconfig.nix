02d84c34ed97e0f795658900148f113269ab6282
Ashwin Balasubramaniyan <ashwin.vbs@gmail.com>
2025-02-17 17:58:42 +0000

    Persist timezone info across reboots
    This allows the timezone to be set to a sane value
    until network services can correct it.

--- components/packageconfig.nix
+++ components/packageconfig.nix
@@ -316,7 +316,7 @@ in
           ({
             hideMounts = true;
             directories = [ "/etc/nixos" "/var/lib/nixos" "/var/log" ];
-            files = [ "/etc/machine-id" ];
+            files = [ "/etc/machine-id" "/etc/localtime" ];
             users.ashwin = homePermanence;
           })
 
