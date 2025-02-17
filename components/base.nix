8aa519cc4a6deab2eb828a51ee7aaf24d5dc62d5
Ashwin Balasubramaniyan <ashwin.vbs@gmail.com>
2025-02-17 04:03:04 +0000

    Remove defaultLocale
    Default locale is en_US.UTF-8 anyway

--- components/base.nix
+++ components/base.nix
@@ -25,7 +25,6 @@ in {
 
       # Default timezone and locale
       time.timeZone = lib.mkDefault "America/New_York";
-      i18n.defaultLocale = "en_US.UTF-8";
 
       #################################################################################################
       # Network configuration
