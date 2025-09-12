35d606e4adb0d7ba2de0d1b60c5f957a450657a4
Ashwin Balasubramaniyan <ashwin.vbs@gmail.com>
2025-09-11 20:33:23 -0400

    TEMP: Disable tailscale
    Tailscaled local build runs tests that require ipv6.
    Need to temporarily disable it to build a generation that enables ipv6.

--- components/base.nix
+++ components/base.nix
@@ -39,7 +39,7 @@ in
       services = {
         # Networking/remote access services
         openssh.enable = true;
-        tailscale.enable = true;
+        # tailscale.enable = true;
       };
 
       programs = {
