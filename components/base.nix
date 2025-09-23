89187ea467ee4a8def27ea6c2bf989ca5d77c2b6
Ashwin Balasubramaniyan <ashwin.vbs@gmail.com>
2025-09-23 01:07:50 -0400

    Remove redundant config
    Enabling gnome automatically enables appropriate compositor

--- components/base.nix
+++ components/base.nix
@@ -128,9 +128,6 @@ in
       services.tzupdate.enable = true;
 
       services.xserver = {
-        # Enable the X11 windowing system.
-        enable = true;
-
         # Enable the GNOME Desktop Environment.
         displayManager.gdm.enable = true;
         desktopManager.gnome.enable = true;
