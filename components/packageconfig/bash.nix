829047582c7f3d106813ad44b3b29c55512b39a3
Ashwin Balasubramaniyan <ashwin.vbs@gmail.com>
2025-10-14 02:36:32 -0400

    Remove systemwide PS1 setting.
    This makes it hard to distinguish between user and root prompts

--- components/packageconfig/bash.nix
+++ components/packageconfig/bash.nix
@@ -4,11 +4,6 @@
   config = {
     environment.sessionVariables.HISTCONTROL = "erasedups:ignoreboth";
 
-    programs.bash = {
-      promptInit = ''
-        PS1="[\[\e[01;32m\]\h\[\e[m\] \[\e[01;34m\]\w\[\e[m\]]$ "
-      '';
-      shellAliases.code = "codium";
-    };
+    programs.bash.shellAliases.code = "codium";
   };
 }
