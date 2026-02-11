349b8ada23dff6f0c561918bf02d5f425778910f
Ashwin Balasubramaniyan <ashwin.vbs@gmail.com>
2026-02-11 03:17:43 -0500

    Do not pre-configure vscode
    It is too much of a hassle to preconfigure extensions..
    Best to manually install per user profile, and add recommended
    extensions in project config.

--- components/packageconfig/devtools.nix
+++ components/packageconfig/devtools.nix
@@ -41,34 +41,11 @@
     # TODO: Make this configurable
     (lib.mkIf config.installconfig.workstation_components {
       # IDE configuration
-      environment.systemPackages = with pkgs; [
-        (vscode-with-extensions.override {
-          vscode = vscodium;
-          vscodeExtensions = (
-            [
-              vscode-extensions.alefragnani.bookmarks
-            ] ++ vscode-utils.extensionsFromVscodeMarketplace [{
-              name = "gitstash";
-              publisher = "arturock";
-              version = "5.2.0";
-              sha256 = "sha256-IVWb4tXD+5YbqJv4Ajp0c3UvYdMzh83NlyiYpndclEY=";
-            }]
-          ) ++ lib.optionals config.installconfig.devtools (
-            [
-              vscode-extensions.denoland.vscode-deno
-              vscode-extensions.jnoortheen.nix-ide
-              vscode-extensions.rust-lang.rust-analyzer
-              vscode-extensions.vadimcn.vscode-lldb
-              vscode-extensions.llvm-vs-code-extensions.vscode-clangd
-            ] ++ vscode-utils.extensionsFromVscodeMarketplace [{
-              name = "geminicodeassist";
-              publisher = "Google";
-              version = "2.37.0";
-              sha256 = "sha256-oJmWxdEN2uTo5Ms3WFrTbosd+DKQq4jrOtPChQjaWe0=";
-            }]
-          );
-        })
-      ];
+      programs.vscode = {
+        enable = true;
+        package = pkgs.vscodium;
+        defaultEditor = true;
+      };
     })
   ];
 }
