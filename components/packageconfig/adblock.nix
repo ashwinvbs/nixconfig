75b188439092cbaa8342249b119faf804a25ddc9
Ashwin Balasubramaniyan <ashwin.vbs@gmail.com>
2026-04-05 15:12:54 -0400

    Use updated blocky config

--- components/packageconfig/adblock.nix
+++ components/packageconfig/adblock.nix
@@ -6,7 +6,7 @@
       ports.dns = 53; # Ensure Blocky is on the standard port
 
       # Use dns over https by default.
-      upstream.default = [
+      upstreams.groups.default = [
         "https://1.1.1.1/dns-query"
         "https://1.0.0.1/dns-query"
       ];
@@ -15,7 +15,6 @@
         "tcp-tls:1.1.1.1:853"
         "tcp-tls:1.0.0.1:853"
       ];
-      startVerifyUpstream = true;
     };
   };
 }
