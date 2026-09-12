f5db23401c7cf34985dbe6fb24084e81b74750b9
Ashwin Balasubramaniyan <ashwin.vbs@gmail.com>
2026-09-12 01:06:45 -0400

    Add code-server on headless dev machines

--- components/packageconfig/devtools.nix
+++ components/packageconfig/devtools.nix
@@ -49,5 +49,58 @@
       environment.systemPackages = with pkgs; [ vscodium.fhs ];
       programs.bash.shellAliases.code = "codium";
     })
+
+    # TODO: make this user independent.
+    (lib.mkIf (!config.installconfig.workstation_components) {
+      services.code-server = {
+        enable = true;
+        user = "ashwin";
+        group = "users";
+        host = "0.0.0.0";
+        port = 8000;
+        auth = "password";
+        disableTelemetry = true;
+        disableUpdateCheck = true;
+      };
+
+      systemd.services.code-server = {
+        path = with pkgs; [
+          coreutils
+          openssl
+        ];
+
+        preStart = ''
+          mkdir -p /home/ashwin/.config/code-server
+
+          # 1. Generate a random 16-character hex password
+          PASSWORD=$(head -c 16 /dev/urandom | od -A n -t x1 | tr -d ' \n')
+
+          # 2. Overwrite the config file dynamically
+          cat <<EOF > /home/ashwin/.config/code-server/config.yaml
+          bind-addr: 0.0.0.0:8000
+          auth: password
+          password: $PASSWORD
+          cert: true
+          EOF
+
+          chown -R ashwin:users /home/ashwin/.config/code-server
+          chmod 600 /home/ashwin/.config/code-server/config.yaml
+
+          # 3. Write the banner to the directory NixOS already created for us (/run/ashwin)
+          echo -e "\n\033[1;34m====================================================\033[0m" > /run/ashwin/ssh-welcome
+          echo -e "\033[1;32m🚀 code-server IDE is ready!\033[0m" >> /run/ashwin/ssh-welcome
+          echo -e "\033[1;37mURL:      http://${config.networking.hostName}:8000\033[0m" >> /run/ashwin/ssh-welcome
+          echo -e "\033[1;37mPassword: $PASSWORD\033[0m" >> /run/ashwin/ssh-welcome
+          echo -e "\033[1;34m====================================================\033[0m\n" >> /run/ashwin/ssh-welcome
+        '';
+      };
+
+      environment.interactiveShellInit = ''
+        if [ -f /run/ashwin/ssh-welcome ]; then
+          cat /run/ashwin/ssh-welcome
+        fi
+      '';
+
+    })
   ];
 }
